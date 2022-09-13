eb64e39ad4abb7576beb5c4ad75020db58526a9d
Itoromfon <itoromfon746@gmail.com>
2022-09-13 18:58:14 +0100

    commit

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const bar = document.getElementById("bar")
 const close = document.getElementById("close")
 const nav = document.getElementById("navbar")
-const topButton = document.getElementById("gotopbtn")
-const home = document.getElementById("home")
+const topBtn = document.getElementById("gotopbtn")
 
 if (bar) {
     bar.addEventListener("click", function() {
@@ -21,6 +20,17 @@ if (nav) {
         nav.classList.remove("active")
     })
 }
+
+function topButton() {
+    let timer = setInterval(() => {
+        if (window.pageYOffset > 50) {
+            topBtn.style.display = "block"
+        } else {
+            topBtn.style.display = "none"
+        }
+    }, 250)
+}
+topButton();
 
 
 
